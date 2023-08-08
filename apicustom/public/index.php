8844baa57e58f7d9b518ea1b3da2de03ccae8712
Veronika Mishchuk <ist201_mvs@student.ztu.edu.ua>
2023-08-08 11:14:03 +0300

    Hometask for 2023-08-08

--- apicustom/public/index.php
+++ apicustom/public/index.php
@@ -26,8 +26,42 @@ $database = new Database($CoreParams['Database']['Host'],
 $database->connect();
 
 $query = new QueryBuilder();
+// Приклад запиту SELECT
 $query->from("news")
     ->select(["title","text"])
     ->where(['id'=>5]);
 $rows = $database->execute($query);
 var_dump($rows);
+// Приклад запиту INSERT
+$dataToInsert = array(
+    'title' => 'Sample Title',
+    'text' => 'Sample Text',
+    'date' => '2023-01-01 00:00:00'  // Поточна дата та час у форматі MySQL
+);
+$query->insert($dataToInsert)
+    ->from("news");
+$database->execute($query);
+
+// Приклад запиту UPDATE
+$dataToUpdate = array(
+    'title' => 'Updated Title',
+    'text' => 'Updated Text',
+    'date' => date('Y-m-d H:i:s')  // Поточна дата та час у форматі MySQL
+);
+$query->update($dataToUpdate)
+    ->from("news")
+    ->where(['id' => 5]);
+$database->execute($query);
+
+// Приклад запиту DELETE
+$query->delete()
+    ->from("news")
+    ->where(['id' => 5]);
+$database->execute($query);
+
+
+// Приклад запиту SELECT з JOIN
+$query->select(['news.title', 'categories.name AS category_name'])
+    ->from('news')
+    ->join('categories', 'news.category_id = categories.id')
+    ->where(['news.id' => 5]);
